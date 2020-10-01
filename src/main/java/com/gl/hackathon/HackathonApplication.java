007697e419ee48cacffa87dbcf521e24c98e3f27
punith <punithklk79@gmail.com>
2020-10-01 21:56:56 +0530

    Add java 1.8 instead of 11, and add config package

--- src/main/java/com/gl/hackathon/HackathonApplication.java
+++ src/main/java/com/gl/hackathon/HackathonApplication.java
@@ -2,8 +2,10 @@ package com.gl.hackathon;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan
 public class HackathonApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/gl/hackathon/config/MvcWebApplicationInitializer.java
@@ -0,0 +1,4 @@
+package com.gl.hackathon.config;
+
+public class MvcWebApplicationInitializer {
+}
